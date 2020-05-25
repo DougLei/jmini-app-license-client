5f8869a9193d4a402dc7eae3ddf3dd16c21dad58
DougLei <953034206@qq.com>
2020-05-25 18:52:51 +0800

    fm

---
+++ src/main/java/com/douglei/mini/license/client/LicenseKeyConstants.java
@@ -0,0 +1,28 @@
+package com.douglei.mini.license.client;
+
+/**
+ * 授权文件key值常量
+ * @author DougLei
+ */
+public class LicenseKeyConstants {
+	
+	/**
+	 * 类型
+	 */
+	public static final String KEY_TYPE = "type";
+	
+	/**
+	 * 有效期
+	 */
+	public static final String KEY_EXPIRE = "expire";
+	
+	/**
+	 * ip地址
+	 */
+	public static final String KEY_IP = "ip";
+	
+	/**
+	 * mac地址
+	 */
+	public static final String KEY_MAC = "mac";
+}
