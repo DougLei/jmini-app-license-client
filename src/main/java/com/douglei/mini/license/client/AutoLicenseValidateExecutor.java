6a3c91a39c4c902b573fd413f8c8737b6c709fee
DougLei <953034206@qq.com>
2020-06-01 17:51:29 +0800

    t

--- src/main/java/com/douglei/mini/license/client/AutoLicenseValidateExecutor.java
+++ src/main/java/com/douglei/mini/license/client/AutoLicenseValidateExecutor.java
@@ -7,7 +7,6 @@ import java.util.Calendar;
  * @author DougLei
  */
 class AutoLicenseValidateExecutor extends Thread{
-	private final long defaultSleep = 1000*60*60*24;
 	private AutoLicenseValidator validator;
 	
 	public AutoLicenseValidateExecutor(String name, AutoLicenseValidator validator) {
@@ -18,23 +17,19 @@ class AutoLicenseValidateExecutor extends Thread{
 	@Override
 	public void run() {
 		while(true) {
-			// TODO 要计算出具体要sleep多久
 			try {
-//				Thread.sleep(sleep());
-				Thread.sleep(5000);
+				Thread.sleep(sleep());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
-			System.out.println("========> 验证结果：" + validator.getResult());
-			System.out.println("========> 剩余天数： " + validator.getLeftDays());
-			
 			if(validator.autoVerify() != null)
 				break;
+			System.out.println("========> 验证结果：" + validator.getResult());
+			System.out.println("========> 剩余天数： " + validator.getLeftDays());
 		}
 	}
 	
-	// 获取需要sleep的毫秒数
+	// 获取需要sleep的毫秒数, 获得当前日期到零点之间的毫秒数, 并加2000毫秒延迟
 	private long sleep() {
 		Calendar calendar = Calendar.getInstance();
 		calendar.add(Calendar.DAY_OF_MONTH, 1);
@@ -42,16 +37,6 @@ class AutoLicenseValidateExecutor extends Thread{
 	    calendar.set(Calendar.MINUTE, 0);
 	    calendar.set(Calendar.SECOND, 0);
 	    calendar.set(Calendar.MILLISECOND, 0);
-		return calendar.getTimeInMillis() - System.currentTimeMillis();
-	}
-	
-	public static void main(String[] args) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DAY_OF_MONTH, 0);
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-	    calendar.set(Calendar.MINUTE, 0);
-	    calendar.set(Calendar.SECOND, 0);
-	    calendar.set(Calendar.MILLISECOND, 0);
-		System.out.println(calendar.getTimeInMillis() - System.currentTimeMillis());
+		return calendar.getTimeInMillis() - System.currentTimeMillis() + 2000;
 	}
 }
